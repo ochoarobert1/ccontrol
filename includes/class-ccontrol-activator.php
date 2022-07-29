7e5c06440d7228d7a560d82fafe70d9bc6eb31c4
Robert Ochoa <ochoa.robert1@gmail.com>
2022-07-29 12:49:16 -0400

    First Commit

---
+++ includes/class-ccontrol-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://robertochoaweb.com/
+ * @since      1.0.0
+ *
+ * @package    Ccontrol
+ * @subpackage Ccontrol/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Ccontrol
+ * @subpackage Ccontrol/includes
+ * @author     Robert Ochoa <ochoa.robert1@gmail.com>
+ */
+class Ccontrol_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-ccontrol-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://robertochoaweb.com/
+ * @since      1.0.0
+ *
+ * @package    Ccontrol
+ * @subpackage Ccontrol/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Ccontrol
+ * @subpackage Ccontrol/includes
+ * @author     Robert Ochoa <ochoa.robert1@gmail.com>
+ */
+class Ccontrol_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
