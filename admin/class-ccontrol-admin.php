20f2509283e53c039878f878d0204841da1f6236
Robert Ochoa <ochoa.robert1@gmail.com>
2022-07-30 21:24:21 -0400

    Pdf Generado

--- admin/class-ccontrol-admin.php
+++ admin/class-ccontrol-admin.php
@@ -109,19 +109,26 @@ class Ccontrol_Admin
     public function ccontrol_create_pdf_callback()
     {
         /*
-        if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
-            error_reporting( E_ALL );
-            ini_set( 'display_errors', 1 );
+        if (defined('WP_DEBUG') && WP_DEBUG) {
+            error_reporting(E_ALL);
+            ini_set('display_errors', 1);
         }
         */
 
+        if (isset($_GET['postid'])) {
+            $postid = $_GET['postid'];
+            $presupuesto = get_post($postid);
+        } else {
+            $presupuesto = 'presupuesto';
+        }
+
         require_once __DIR__ . '/../vendor/autoload.php';
 
-        $pdf = new FPDF();
+        $pdf = new tFPDF();
         $pdf->AddPage();
-        $pdf->SetFont('Arial', 'B', 16);
-        $pdf->Cell(40, 10, '¡Hola, Mundo!');
-        $pdf->Output('I','requerimiento_pdf.pdf');
+        $pdf->SetFont('Helvetica', 'B', 16);
+        $pdf->Cell(40, 10, utf8_decode('¡Hola, Mundo!'));
+        $pdf->Output('I', utf8_decode($presupuesto->post_title) . '.pdf');
         wp_die();
     }
 
--- admin/js/ccontrol-admin.js
+++ admin/js/ccontrol-admin.js
@@ -7,23 +7,7 @@
         $('#printQuote').on('click', function(e) {
             e.preventDefault();
             console.log('clicked');
-
-            jQuery.ajax({
-                type: 'POST',
-                url: ajaxurl,
-                data: {
-                    action: 'ccontrol_create_pdf',
-                    postid: jQuery('#printQuote').data('id')
-                },
-                success: function(result) {
-					console.log(result);
-                    var blob = new Blob([result]);
-                    var link = document.createElement('a');
-                    link.href = window.URL.createObjectURL(blob);
-                    link.download = "test.pdf";
-                    link.click();
-                }
-            });
+            window.open(ajaxurl + '?action=ccontrol_create_pdf&postid=' + jQuery('#printQuote').data('id'),'_blank');
         });
     });
 
